78ffc75ce00ca1fcf6580a37cdaae364e843241e
Abxyz654 <112222015+Abxyz654@users.noreply.github.com>
2025-03-13 16:01:56 +0700

    Update three point problem (input manually).py

--- three point problem (input manually).py
+++ three point problem (input manually).py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-# abqari was here 11/03/2025 45 minutes
+
 def get_point(name):
     x = float(input(f"\033[96mMasukkan koordinat X untuk {name}:\033[0m "))
     y = float(input(f"\033[96mMasukkan koordinat Y untuk {name}:\033[0m "))
